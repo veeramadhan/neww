ab18a6867dcd2ed040bc15650c1a2f5b1c6afb65
veeramadhan <95845583+veeramadhan@users.noreply.github.com>
2023-08-11 19:40:05 +0530

    resume

--- public/Resume.js
+++ public/Resume.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ResumeButton = () => {
   const handleDownloadResume = () => {
     // Replace 'resume.pdf' with the actual filename of your resume file.
-    const resumeFilePath = '/Veeramanikandan__Curriculum Vitae.pdf';
+    const resumeFilePath = '/Veera_Resume.pdf';
 
     // Create a temporary anchor element to trigger the download.
     const downloadLink = document.createElement('a');
     downloadLink.href = resumeFilePath;
-    downloadLink.download = 'C:\VEERA\ReactJS\Website\resume\public\Veeramanikandan__Curriculum Vitae.pdf'; // Change the filename if needed.
+    downloadLink.download = 'C:\VEERA\ReactJS\Website\resume\public\Veera_Resume.pdf'; // Change the filename if needed.
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
